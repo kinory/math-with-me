af6be14bfe3ede6b09568f03e81cde21f4e8ff09
dvky16 <dvky16@64986d46-9cb6-4abf-b4cc-81a675aebe78>
2016-04-15 20:47:43 +0000

    --

--- MathWithMe/app/src/main/java/kkkk/mathwithme/activity/RoomActivity.java
+++ MathWithMe/app/src/main/java/kkkk/mathwithme/activity/RoomActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import kkkk.mathwithme.R;
 import kkkk.mathwithme.model.Exercise;
 import kkkk.mathwithme.model.ExerciseGenerator;
+import kkkk.mathwithme.model.LinearEquation;
+import kkkk.mathwithme.model.QuadraticEquation;
 import kkkk.mathwithme.model.server.ServerAPI;
 
 public class RoomActivity extends AppCompatActivity {
@@ -18,8 +20,17 @@ public class RoomActivity extends AppCompatActivity {
         int type = room.getLevel() % 2 + 1;
         int level = room.getLevel() / 3 + 1;
         Exercise exercise = new ExerciseGenerator(room.getSeed(), type, level).generateExercise();
-        switch (exercise.getType()) {
+        int[] params = exercise.getParameters();
+        if (exercise.getType() == LinearEquation.class) {
+            if (params[0] == 0 && params[3] == 0 && params[1]==0) {
 
+            }
+        } else if (exercise.getType() == QuadraticEquation.class) {
+            if (params[2] == 0) {
+                String format = String.format("%d*x^2+%d*x", params[0], params[1]);
+            } else {
+                String format = String.format("%d*x^2+%d*x+%d", params[0], params[1], params[2]);
+            }
         }
     }
 }
