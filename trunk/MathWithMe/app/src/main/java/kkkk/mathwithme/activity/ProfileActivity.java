bf2fb392b0e48eb6c85f9cfca4f12edea49db62d
yonatanka <yonatanka@64986d46-9cb6-4abf-b4cc-81a675aebe78>
2016-04-15 16:47:14 +0000

    continued working

---
+++ trunk/MathWithMe/app/src/main/java/kkkk/mathwithme/activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package kkkk.mathwithme.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kkkk.mathwithme.R;
+
+public class ProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
--- trunk/MathWithMe/app/src/main/java/kkkk/mathwithme/activity/SignUpActivity.java
+++ trunk/MathWithMe/app/src/main/java/kkkk/mathwithme/activity/SignUpActivity.java
@@ -3,9 +3,14 @@ package kkkk.mathwithme.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+
 import kkkk.mathwithme.R;
 
 public class SignUpActivity extends Activity {
+
+    private static final String SERVER_URL = "http://math-with-me.rapidapi.io/sendmessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
