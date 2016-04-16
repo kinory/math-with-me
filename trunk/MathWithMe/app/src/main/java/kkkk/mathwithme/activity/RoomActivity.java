64162d695985d605d709f2d3f5070702ea3f0c5e
Kinory <Kinory@64986d46-9cb6-4abf-b4cc-81a675aebe78>
2016-04-16 01:00:15 +0000

    fghsfj

--- trunk/MathWithMe/app/src/main/java/kkkk/mathwithme/activity/RoomActivity.java
+++ trunk/MathWithMe/app/src/main/java/kkkk/mathwithme/activity/RoomActivity.java
@@ -2,11 +2,12 @@ package kkkk.mathwithme.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import io.github.kexanie.library.MathView;
 import kkkk.mathwithme.R;
+import kkkk.mathwithme.model.ChatRoomAdapter;
 import kkkk.mathwithme.model.Exercise;
 import kkkk.mathwithme.model.ExerciseGenerator;
 import kkkk.mathwithme.model.LinearEquation;
@@ -21,6 +22,9 @@ public class RoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new ChatRoomAdapter(this));
+
         MathView mathView = (MathView) findViewById(R.id.mathView);
         exerciseTextView = new TextView(this);
         ServerAPI.Room room = (ServerAPI.Room) getIntent().getSerializableExtra("room");
